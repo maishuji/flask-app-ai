1e990949d5b8e874637994b0f2831973812f5497
Quentin Cartier <daoliangshumaishuji@gmail.com>
2024-12-14 21:57:17 +0100

    Add rest endpoints

--- server.py
+++ server.py
@@ -1,8 +1,171 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
+
+# Some data  simulating database
+data = [
+    {
+        "id": "3b58aade-8415-49dd-88db-8d7bce14932a",
+        "first_name": "Tanya",
+        "last_name": "Slad",
+        "graduation_year": 1996,
+        "address": "043 Heath Hill",
+        "city": "Dayton",
+        "zip": "45426",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/139x100.png/cc0000/ffffff",
+    },
+    {
+        "id": "d64efd92-ca8e-40da-b234-47e6403eb167",
+        "first_name": "Ferdy",
+        "last_name": "Garrow",
+        "graduation_year": 1970,
+        "address": "10 Wayridge Terrace",
+        "city": "North Little Rock",
+        "zip": "72199",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/148x100.png/dddddd/000000",
+    },
+    {
+        "id": "66c09925-589a-43b6-9a5d-d1601cf53287",
+        "first_name": "Lilla",
+        "last_name": "Aupol",
+        "graduation_year": 1985,
+        "address": "637 Carey Pass",
+        "city": "Gainesville",
+        "zip": "32627",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/174x100.png/ff4444/ffffff",
+    },
+    {
+        "id": "0dd63e57-0b5f-44bc-94ae-5c1b4947cb49",
+        "first_name": "Abdel",
+        "last_name": "Duke",
+        "graduation_year": 1995,
+        "address": "2 Lake View Point",
+        "city": "Shreveport",
+        "zip": "71105",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/145x100.png/dddddd/000000",
+    },
+    {
+        "id": "a3d8adba-4c20-495f-b4c4-f7de8b9cfb15",
+        "first_name": "Corby",
+        "last_name": "Tettley",
+        "graduation_year": 1984,
+        "address": "90329 Amoth Drive",
+        "city": "Boulder",
+        "zip": "80305",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/198x100.png/cc0000/ffffff",
+    }
+]
 
 @app.route("/")
 def home():
     return {"message" : "Hello, World!"}
 
+# Define a route for the "/no_content" URL
+@app.route("/no_content")
+def no_content():
+    """Return 'no content found' with a status of 204.
+    Returns:
+        tuple: A tuple containing a dictionary and a status code.
+    """
+    # Create a dictionary with a message and return it with a 204 No Content status code
+    return ({"message": "No content found"}, 204)
+
+# Define a route for the "/exp" URL
+@app.route("/exp")
+def index_explicit():
+    """Return 'Hello World' message with a status code of 200.
+    Returns:
+        response: A response object containing the message and status code 200.
+    """
+    # Create a response object with the message "Hello World"
+    resp = make_response({"message": "Hello World"})
+    # Set the status code of the response to 200
+    resp.status_code = 200
+    # Return the response object
+    return resp
+
+@app.route("/data")
+def get_data():
+    try:
+        # Check if 'data' exists and has a length greater than 0
+        if data and len(data) > 0:
+            # Return a JSON response with a message indicating the length of the data
+            return {"message": f"Data of length {len(data)} found"}
+        else:
+            # If 'data' is empty, return a JSON response with a 500 Internal Server Error status code
+            return {"message": "Data is empty"}, 500
+    except NameError:
+        # Handle the case where 'data' is not defined
+        # Return a JSON response with a 404 Not Found status code
+        return {"message": "Data not found"}, 404
+
+@app.route("/name_search")
+def name_search():
+    """Find a person in the database
+    
+    Returns:
+        json: Person if found, with status of 200
+        404: If not found
+        422: If argument 'q' is missing
+    """
+    query = request.args.get('q')
+    
+    # Check if the query parameter q is missing
+    if not query:
+        return {"message" : "Query parameter 'q' is missing"}, 422
+    
+    for person in data:
+        if query.lower() in person["first_name"].lower():
+            return person
+    
+    return {"message": "Person not found"}, 404
+
+@app.route("/count")
+def get_count():
+    if data:
+        return {"length" : len(data)}, 200
+    else:
+        return {"length" : 0}, 200
+    
+@app.route("/person/<uuid:id>", methods=['GET'])
+def find_by_uuid(id):
+    for person in data:
+        if person["id"] == str(id):
+            return person
+    return {"message": "Person not found"}, 404
+
+@app.route("/person/<uuid:id>", methods=['DELETE'])
+def delete_person(id):
+        for person in data:
+            if person["id"] == str(id):
+                # Remove the person from the data list
+                data.remove(person)
+                return {"message": "Person with ID deleted"}, 200
+        return {"message": "Person not found"}, 404
+    
+@app.route("/person", methods=['POST'])
+def create_person():
+    new_person = request.get_json()
+    
+    if not new_person:
+        return {"message": "Invalid input, no data provided"}, 400
+    for person in data:
+        if person["id"] == new_person["id"]:
+            return {"message": "Cannot create person. UUID already exists"}, 403
+        
+    try:
+        data.append(new_person)
+    except NameError:
+        return {"message": "data not defined"}, 500
+    return {"message": "Person created successfully"}, 201
+
+@app.errorhandler(404)
+def api_not_found(error):
+    # This function is a custom error handler for 404 Not Found errors
+    # It is triggered whenever a 404 error occurs within the Flask application
+    return {"message": "API not found"}, 404
